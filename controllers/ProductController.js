e7f4b38bacd2963ac52db06d9187ea47c9ffabdb
Abdoul93230 <117718513+Abdoul93230@users.noreply.github.com>
2024-12-02 02:13:04 -0800

    Update controllers/ProductController.js [BoltSync]

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -5,6 +5,7 @@ const createProduct = async (req, res) => {
   try {
     const data = req.body;
     let pictures = [];
+    let variantImages = new Map();
 
     // Gestion des images principales
     if (req.files) {
@@ -29,20 +30,37 @@ const createProduct = async (req, res) => {
         );
         pictures.push(result3.secure_url);
       }
-    }
 
-    // Gestion des images additionnelles
-    if (req.files.nouveauChampImages) {
-      for (const file of req.files.nouveauChampImages) {
-        const result = await cloudinary.uploader.upload(file.path, {
-          folder: "images",
-        });
-        pictures.push(result.secure_url);
+      // Gestion des images des variantes
+      if (req.files.variantImages) {
+        for (const file of req.files.variantImages) {
+          const result = await cloudinary.uploader.upload(file.path, {
+            folder: "variants",
+          });
+          variantImages.set(file.originalname, result.secure_url);
+        }
+      }
+
+      // Gestion des images additionnelles
+      if (req.files.nouveauChampImages) {
+        for (const file of req.files.nouveauChampImages) {
+          const result = await cloudinary.uploader.upload(file.path, {
+            folder: "images",
+          });
+          pictures.push(result.secure_url);
+        }
       }
     }
 
     // Traitement des variantes
     const variants = data.variants ? JSON.parse(data.variants) : [];
+    const processedVariants = variants.map(variant => ({
+      color: variant.colorName,
+      colorCode: variant.color,
+      sizes: variant.sizes,
+      imageUrl: variantImages.get(variant.imageFile.name) || '',
+      stock: 2,
+    }));
 
     // Création du produit avec les variantes
     const product = new Produit({
@@ -52,14 +70,7 @@ const createProduct = async (req, res) => {
       prix: data.prix,
       prixf: data.prixF || 0,
       description: data.description,
-      variants: variants.map((variant) => ({
-        color: variant.colorName,
-        colorCode: variant.color,
-        sizes: variant.sizes,
-        imageUrl: variant.imageUrl,
-        // stock: Math.floor(data.quantite / variants.length), // Distribution équitable du stock
-        stock: 2,
-      })),
+      variants: processedVariants,
       marque: data.marque,
       ClefType: data.ClefType,
       Clefournisseur: data.Clefournisseur,
@@ -78,178 +89,6 @@ const createProduct = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: "Une erreur s'est produite lors de la création du produit",
-      error: error.message,
-    });
-  }
-};
-
-const updateProduct = async (req, res) => {
-  try {
-    const productId = req.params.productId;
-    const data = req.body;
-    const product = await Produit.findById(productId);
-
-    if (!product) {
-      return res.status(404).json({ message: "Produit introuvable" });
-    }
-
-    let updateData = {
-      name: data.name,
-      quantite: data.quantite,
-      prixPromo: data.prixPromo,
-      prix: data.prix,
-      prixf: data.prixF || 0,
-      description: data.description,
-      marque: data.marque,
-      ClefType: data.ClefType,
-      Clefournisseur: data.Clefournisseur,
-      prixLivraison: data.prixLivraison || 0,
-    };
-
-    // Mise à jour des variantes
-    if (data.variants) {
-      const variants = JSON.parse(data.variants);
-      updateData.variants = variants.map((variant) => ({
-        color: variant.colorName,
-        colorCode: variant.color,
-        sizes: variant.sizes,
-        imageUrl: variant.imageUrl,
-        stock: 1,
-      }));
-    }
-
-    // Gestion des images
-    if (req.files) {
-      const deleteImageFromCloudinary = async (url) => {
-        if (!url) return;
-        const publicId = url.split("/").pop().split(".")[0]; // Récupérer le `public_id` de l'URL
-        await cloudinary.uploader.destroy(`images/${publicId}`); // Suppression
-      };
-
-      if (req.files.image1) {
-        await deleteImageFromCloudinary(product.image1); // Supprimer l'ancienne image
-        const result1 = await cloudinary.uploader.upload(
-          req.files.image1[0].path,
-          { folder: "images" }
-        );
-        updateData.image1 = result1.secure_url;
-      }
-
-      if (req.files.image2) {
-        await deleteImageFromCloudinary(product.image2);
-        const result2 = await cloudinary.uploader.upload(
-          req.files.image2[0].path,
-          { folder: "images" }
-        );
-        updateData.image2 = result2.secure_url;
-      }
-
-      if (req.files.image3) {
-        await deleteImageFromCloudinary(product.image3);
-        const result3 = await cloudinary.uploader.upload(
-          req.files.image3[0].path,
-          { folder: "images" }
-        );
-        updateData.image3 = result3.secure_url;
-      }
-
-      if (req.files.nouveauChampImages) {
-        const newPictures = [];
-        for (const file of req.files.nouveauChampImages) {
-          newPictures.push(
-            (await cloudinary.uploader.upload(file.path, { folder: "images" }))
-              .secure_url
-          );
-        }
-
-        // Supprimer les anciennes images du champ "pictures"
-        if (product.pictures && product.pictures.length > 0) {
-          for (const oldUrl of product.pictures) {
-            await deleteImageFromCloudinary(oldUrl);
-          }
-        }
-
-        updateData.pictures = newPictures;
-      }
-    }
-
-    const updatedProduct = await Produit.findByIdAndUpdate(
-      productId,
-      updateData,
-      { new: true }
-    );
-
-    return res.json({
-      message: "Produit mis à jour avec succès",
-      data: updatedProduct,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      message: "Erreur lors de la mise à jour du produit",
-      error: error.message,
-    });
-  }
-};
-
-const deleteProduct = async (req, res) => {
-  try {
-    const productId = req.params.productId;
-    const product = await Produit.findById(productId);
-
-    if (!product) {
-      return res.status(404).json({ message: "Produit introuvable" });
-    }
-
-    // Suppression des images de Cloudinary
-    const imagesToDelete = [
-      product.image1,
-      product.image2,
-      product.image3,
-      ...product.pictures,
-      ...product.variants.map((v) => v.imageUrl),
-    ].filter(Boolean);
-
-    for (const imageUrl of imagesToDelete) {
-      const publicId = `images/${imageUrl.split("/").pop().split(".")[0]}`;
-      await cloudinary.uploader.destroy(publicId);
-    }
-
-    await Produit.findByIdAndRemove(productId);
-    return res.json({ message: "Produit supprimé avec succès" });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur lors de la suppression du produit",
-      error: error.message,
-    });
-  }
-};
-
-const getProductById = async (req, res) => {
-  try {
-    const product = await Produit.findById(req.params.productId);
-    if (!product) {
-      return res.status(404).json({ message: "Produit introuvable" });
-    }
-    return res.json({ data: product });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur lors de la recherche du produit",
-      error: error.message,
-    });
-  }
-};
-
-const getAllProducts = async (req, res) => {
-  try {
-    const products = await Produit.find();
-    if (products.length === 0) {
-      return res.status(404).json({ message: "Aucun produit pour le moment." });
-    }
-    return res.json({ data: products });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur lors de la récupération des produits",
       error: error.message,
     });
   }
